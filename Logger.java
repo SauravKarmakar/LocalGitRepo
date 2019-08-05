a5763f10d4858e7a9d0d50adc18baa4fff6cdba7
Saurav Karmakar <saurav.23k@gmail.com>
2019-08-06 00:09:21 +0530

    Modifying both Logger and MainApplication .java

--- Logger.java
+++ Logger.java
@@ -1,8 +1,13 @@
 public class Logger {
-    
+
+    private enum LoggingType { INFO, DEBUG, FINE, FINEST}
     private String loggingType = "INFO";
 
-    public Logger(){}
+    public Logger() {
+        this.loggingType = LoggingType.INFO;
+    }
 
     public String getLoggerType() { return loggingType; }
+
+    public void setLoggerType(String loggingType) {}
 }
--- MainApplication.java
+++ MainApplication.java
@@ -2,4 +2,9 @@ public class MainApplication {
     public static void main(Stringp[] args) {
         System.out.println("Hello World");
     }
+
+    // common method for logging
+    private static logging(String mesg){
+        System.out.println("LOGGER: " + mesg);
+    }
 }
