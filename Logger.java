8e8b7ef3dbbdf3c24c4af38f4390af882d01c09f
Saurav Karmakar <saurav.23k@gmail.com>
2019-08-06 00:17:30 +0530

    Added isDebugEnabled method

--- Logger.java
+++ Logger.java
@@ -2,12 +2,16 @@ public class Logger {
 
     private enum LoggingType { INFO, DEBUG, FINE, FINEST}
     private String loggingType = "INFO";
+    private boolean debug = false;
 
     public Logger() {
         this.loggingType = LoggingType.INFO;
+        this.debug = false;
     }
 
     public String getLoggerType() { return loggingType; }
 
     public void setLoggerType(String loggingType) {}
+
+    public boolean isDebugEnabled() { return debug; }
 }
